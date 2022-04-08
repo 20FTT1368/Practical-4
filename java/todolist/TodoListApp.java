bc9b226ceff89070dbb9434d55b9ea840f9312ce
Fiqahhassan <fiqah.hjhassan@gmail.com>
2022-04-08 13:59:57 +0800

    Lesson 1.1 - setting up JavaFX project using Maven

---
+++ java/todolist/TodoListApp.java
@@ -0,0 +1,17 @@
+package todolist;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TodoListApp extends Application {
+	@Override
+	public void start(Stage arg0) throws Exception {
+		arg0.setTitle("Todo List App");
+		arg0.setResizable(false);
+		arg0.sizeToScene();
+		arg0.show();
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+}
